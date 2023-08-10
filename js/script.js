1dea45e325cbbf68db311b5189ad22393a8b312e
joaogorayeb <jo.bt@hotmail.com>
2023-08-10 16:37:43 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let adc = document.getElementById('add');
+
+adc.addEventListener('click', function() {
+    let divRtgl = document.createElement('div');
+    divRtgl.classList.add('retangulo');
+
+    document.getElementById('retangulos').appendChild(divRtgl);
+});
+document.getElementById('sub').addEventListener('click', function (){
+    let divsRetangulo = document.querySelectorAll('.retangulo');
+    if(divsRetangulo.length > 0) {
+        divsRetangulo[0].remove();
+    }
+});
