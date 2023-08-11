1adde92c34c4a30dbc9bdbee046ce24086ab1d92
joaogorayeb <jo.bt@hotmail.com>
2023-08-11 16:57:20 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 let adc = document.getElementById('add');
 
 adc.addEventListener('click', function() {
+    let divsRetangulo = document.querySelectorAll('.retangulo');
     let divRtgl = document.createElement('div');
     divRtgl.classList.add('retangulo');
+    divRtgl.innerText = divsRetangulo.length +1
 
     document.getElementById('retangulos').appendChild(divRtgl);
 });
 document.getElementById('sub').addEventListener('click', function (){
     let divsRetangulo = document.querySelectorAll('.retangulo');
+    console.log (divsRetangulo);
     if(divsRetangulo.length > 0) {
-        divsRetangulo[0].remove ();
+        divsRetangulo[divsRetangulo.length -1].remove ();
     }
 });
